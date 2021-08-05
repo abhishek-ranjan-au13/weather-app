4e29d7ac6e431053db709c455d4712b00c41c689
abhishek-ranjan-au13 <gayanvjy27@protonmail.com>
2021-08-05 16:38:22 +0530

    add humidity and cloud cover

--- src/utils/forcast.js
+++ src/utils/forcast.js
@@ -1,12 +1,3 @@
-//
-// Goal: Create a reusable function for getting the forecast
-//
-// 1. Setup the "forecast" function in utils/forecast.js
-// 2. Require the function in app.js and call it as shown below
-// 3. The forecast function should have three potential calls to callback:
-//    - Low level error, pass string for error
-//    - Coordinate error, pass string for error
-//    - Success, pass forecast string for data (same format as from before)
 const request = require("postman-request");
 const forecast = (latitude, longitude, callback) => {
   const url =
@@ -14,15 +5,15 @@ const forecast = (latitude, longitude, callback) => {
     latitude +
     "," +
     longitude;
-  request({ url: url, json: true }, (error, response) => {
+  request({ url: url, json: true }, (error, { body }) => {
     if (error) {
       callback("unable to get weather-app", undefined);
-    } else if (response.body.error) {
+    } else if (body.error) {
       callback("unable to find the location", undefined);
     } else {
       callback(
         undefined,
-        ` ${response.body.current.weather_descriptions} . It is currently ${response.body.current.temperature} degrees out, but feels like ${response.body.current.feelslike} degrees`
+        ` ${body.current.weather_descriptions} . It is currently ${body.current.temperature} degrees out, but feels like ${body.current.feelslike} degrees. Humidity is ${body.current.humidity}% and Cloudcover is ${body.current.cloudcover}%`
       );
     }
   });
